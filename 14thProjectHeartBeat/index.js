ff1e4ab77356e497cc4385ae4c835bbcb97b2686
Siddu Panchal <siddupanchal0988@gmail.com>
2024-02-14 19:08:11 +0530

    Heart Beat project added

---
+++ 14thProjectHeartBeat/index.js
@@ -0,0 +1,12 @@
+let grayHeart = document.querySelector(".gray-heart")
+let redHeart = document.querySelector(".red-heart")
+
+grayHeart.addEventListener('click', ()=>{
+    redHeart.classList.add('animation')
+    redHeart.classList.add('fill-color')
+})
+
+redHeart.addEventListener('click', ()=>{
+    redHeart.classList.remove('animation')
+    redHeart.classList.remove('fill-color')
+})
