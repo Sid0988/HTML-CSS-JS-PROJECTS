7e231108daf0f1b3bb82cb197f6ab23fcb8620a7
Siddu Panchal <siddupanchal0988@gmail.com>
2024-02-17 17:24:28 +0530

    added comments

--- 18thProjectVideoTrailerPopup/app.js
+++ 18thProjectVideoTrailerPopup/app.js
@@ -1,8 +1,8 @@
-let playVideo = document.querySelector('.playImg');
-let bgcover = document.querySelector('.nanu')
-let videoconatiner = document.querySelector(".active")
-let closeVideo = document.querySelector('.close-icon')
-let mainVideo = document.querySelector(".atm")
+let playVideo = document.querySelector('.playImg');// this is for play img which is at bottom
+let bgcover = document.querySelector('.nanu') // this is for background color 
+let videoconatiner = document.querySelector(".active") // this is for video div or container
+let closeVideo = document.querySelector('.close-icon') //this is for close icon
+let mainVideo = document.querySelector(".atm")// this is for video
 
 playVideo.addEventListener('click', ()=>{
     playVideo.classList.add("btn2")
